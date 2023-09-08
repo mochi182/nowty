bc4d7c09305a8071653df79b47d63f700a11ec0d
palmbreeze <iamfranciscoperez@gmail.com>
2023-09-08 20:51:58 +0200

    Add activity component style

--- frontend/src/components/AddActivity.jsx
+++ frontend/src/components/AddActivity.jsx
@@ -1,7 +1,30 @@
 import './AddActivity.css'
+import '../assets/Buttons.css'
+import '../assets/Forms.css'
 
 export function AddActivity() {
-    return(
-        <input/>
+    return (
+        <>
+            <form id="addForm" className="">
+
+                <div className="form-group">
+                    <label for="actividad-input">Nueva actividad:</label>
+                    <input className="form-control" type="text" id="actividad-input" name="actividad-input" required />
+                </div>
+
+            </form>
+
+            <center>
+                <button className="customButton primary addActivityButtons" id="addButton">
+                    Add
+                </button>
+
+                <button className="customButton primary addActivityButtons">
+                    Clear
+                </button>
+
+                <a href="advanced" className="overrideLinkStyle customButton outline-primary addActivityButtons">🚀</a>
+            </center>
+        </>
     )
 }
